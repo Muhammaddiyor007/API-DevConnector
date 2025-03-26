a463d9b5206454842b3465bc521eee1331ec358c
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2025-03-26 15:39:20 +0500

    "API-DevConnector"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Route, HashRouter as Router, Routes } from 'react-router-dom'
+import { Route, HashRouter as Router, Routes, Navigate } from 'react-router-dom'
 import { AppContext } from './context/ContexApi'
 import Home from './pages/Home'
 import Dashboard from './pages/dashboard'
@@ -11,6 +11,12 @@ import Profil from './pages/profil'
 import Registration from './pages/register'
 import Header from './components/Header'
 import NotFound from './pages/not-found'
+
+// Protected route component
+const PrivateRoute = ({ element }) => {
+  const { isToken } = useContext(AppContext)
+  return isToken ? element : <Navigate to="/login" replace />
+}
 
 const Layout = ({ children }) => (
   <>
@@ -24,16 +30,19 @@ function App() {
 
   return (
     <Router>
-      <Layout> 
+      <Layout>
         <Routes>
           <Route path="/" element={isToken ? <Developers /> : <Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/developers" element={<Developers />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/post/:id" element={<PostData />} />
+          
+          {/* Protected Routes */}
+          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+          <Route path="/post" element={<PrivateRoute element={<Post />} />} />
+          <Route path="/post/:id" element={<PrivateRoute element={<PostData />} />} />
           <Route path="/profile/:id" element={<Profil />} />
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
